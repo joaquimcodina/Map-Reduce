f22cdf76e6e2cb845f75c7123597a9055fdcccac
1563480 <57755107+1563480@users.noreply.github.com>
2022-03-31 21:23:13 +0200

    Entry and save lines

--- plabMapReduceATS/src/main/java/org/example/Entry.java
+++ plabMapReduceATS/src/main/java/org/example/Entry.java
@@ -4,16 +4,28 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 
 public class Entry {
-    private FileReader entryDades;
+    BufferedReader br;
+
+    List<String> listaArrays;
     private int numCaracters;
+    String file;
 
     Entry() throws FileNotFoundException {
-        entryDades = new FileReader("NombreFichero");
+        file = "fitxer1.txt";
+        listaArrays = new ArrayList<>();
+        br = new BufferedReader(new FileReader(file));
         numCaracters = 0;
-
     }
-    public void emptyFile() {
-
+    public void guardarLinias() throws IOException {
+        String line = "";
+        do {
+            line = br.readLine();
+            if (line == null) {
+                break;
+            }
+            String lineCorrect = line.replaceAll("\\s","");
+            listaArrays.add(lineCorrect);
+        } while(line != null);
     }
     public int getNumCaracters() {
         return numCaracters;
