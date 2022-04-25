c288e1ef70fc45db35ee9eb56991d72d1c6498a5
joaquimcodina <joaquim.codina.espin@gmail.com>
2022-04-25 15:58:29 +0200

    Documentar i optimitzar Classe Shuffle

--- src/Shuffle.java
+++ src/Shuffle.java
@@ -2,33 +2,37 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 
+/**
+ * Aquesta es la Classe Shuffle
+ *
+ * @author Joaquim Codina (1566940) i Marc Cives (1563480)
+ *
+ */
 public class Shuffle {
-    HashMap<String, ArrayList<Integer>> hashMap = new LinkedHashMap<>();
 
+    /**
+     * Aquesta es la funció generarShuffling(), bàsicament s'encarrega de generar un HashMap, i a partir d'un bucle
+     * buscarà per cada caràcter, el nombre de vegades que apareix en el fitxer de text, en cas de que aparegui, afegim un 1
+     * a la ArrayList<Integer> que es troba dins del HashMap, i que més endavant sumarem tot el seu contingut.
+     *
+     * @param llista on s'emmagatzema el contingut del fitxer de text, el qual hem esborrat els caràcters repetits per paraules,
+     *              i l'hem separat per caràcters amb un split.
+     *
+     * @return hashMap on s'emmagatzema els caràcters del fitxer juntament amb el seu nombre d'aparicions (sense sumar).
+     *
+     */
     public HashMap<String, ArrayList<Integer>> generarShuffling(ArrayList<String> llista) {
-        for (String s : llista) {
-            String[] temp = s.toLowerCase()
-                    .replace(" ", "")
-                    .replace(".", "")
-                    .replace(";", "")
-                    .replace("'","")
-                    .replace("-", "")
-                    .replace(":", "")
-                    .replace("[", "")
-                    .replace("]","")
-                    .split(",");
-
-            for (int i = 0; i < temp.length; i++) {
-                ArrayList<Integer> valuesList = hashMap.get(temp[i]);
-                if (!temp[i].equalsIgnoreCase("")) {
-                    if(valuesList == null) {
-                        valuesList = new ArrayList<>();
-                        valuesList.add(1);
-                    } else {
-                        valuesList.add(1);
-                    }
-                    hashMap.put(temp[i], valuesList);
+        HashMap<String, ArrayList<Integer>> hashMap = new LinkedHashMap<>();
+        for (int i = 0; i < llista.size(); i++) {
+            ArrayList<Integer> valuesList = hashMap.get(llista.get(i));
+            if (!llista.get(i).equalsIgnoreCase("")) {
+                if(valuesList == null) {
+                    valuesList = new ArrayList<>();
+                    valuesList.add(1);
+                } else {
+                    valuesList.add(1);
                 }
+                hashMap.put(llista.get(i), valuesList);
             }
         }
         return hashMap;
