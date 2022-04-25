56784745d6e8ba3ef22fd24da0f0ea4785c0490c
joaquimcodina <joaquim.codina.espin@gmail.com>
2022-04-25 15:58:04 +0200

    Documentar i optimitzar Classe Entry

--- src/Entry.java
+++ src/Entry.java
@@ -3,20 +3,36 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 
+/**
+ * Aquesta es la Classe Entry, accedeix al contingut dels fitxers de Text.
+ *
+ * @author Joaquim Codina (1566940) i Marc Cives (1563480)
+ *
+ */
 public class Entry {
     BufferedReader br;
     ArrayList<String> llista;
-    private int numCaracters;
 
+    /**
+     * Aquesta es la funció Entry, que consisteix en inicialitzar un ArrayList<String> on anira el contingut del
+     * nostre fitxer de text, i un BufferedReader, que s'encarrega de llegir el contingut del fitxer de text linia per linia.
+     *
+     * @param file fitxer de text passat per paràmetre d'entrada.
+     *
+     */
     Entry(String file){
         try{
-            numCaracters = 0;
             llista = new ArrayList<>();
             br = new BufferedReader(new FileReader(file));
         } catch (IOException e) {
             System.out.println("Error E/S" + e);
         }
     }
+    /**
+     * Aquesta es la funció guardarLinias(), consisteix en guardar el contingut del fitxer de text linia per linia
+     * en el ArrayList<String>.
+     *
+     */
     public void guardarLinias(){
         try{
             String linea = br.readLine();
@@ -29,7 +45,11 @@ public class Entry {
             System.out.println("Error: " + e);
         }
     }
-    public int getNumCaracters() { return numCaracters; }
-    public void setNumCaracters(int numC) { numCaracters = numC;}
+    /**
+     * Aquesta es la funció getLines(), retorna el ArrayList<String> on tenim emmagatzemat el contingut del fitxer.
+     *
+     * @return llista.
+     *
+     */
     public ArrayList<String> getLines(){ return llista; };
 }
