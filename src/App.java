0facc87b8be4a8f978360f19bfe8b2642d6831fa
joaquimcodina <joaquim.codina.espin@gmail.com>
2022-04-25 15:57:33 +0200

    Documentar i optimitzar Classe App

--- src/App.java
+++ src/App.java
@@ -2,33 +2,35 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 /**
- * Hello world!
+ * Aquesta es la Classe principal, on fem crides a les diferents funcions de les altres classes per dur a terme
+ * l'algoritme MapReduce
+ *
+ * @author Joaquim Codina (1566940) i Marc Cives (1563480)
  *
  */
 public class App
 {
     public static void main(String[] args) {
+
         for (String file : args) {
             Entry entry = new Entry(file);
             entry.guardarLinias();
             ArrayList<String> llista = entry.getLines();
-            ArrayList<String> llistalLength = entry.getLines();
 
             Split split = new Split();
-            split.actualitzarLista(llista);
-            llista = split.getArrayCharacters();
+            llista = split.generatesplitWord(llista);
 
             Map map = new Map();
-            map.generarHashMap(llista);
+            ArrayList<String> characterList = map.splitCharacter(llista);
 
             Shuffle shuffle = new Shuffle();
-            HashMap<String, ArrayList<Integer>> hashMap = shuffle.generarShuffling(llista);
+            HashMap<String, ArrayList<Integer>> hashMap = shuffle.generarShuffling(characterList);
 
             Reduce reduce = new Reduce();
             hashMap = reduce.generarReducing(hashMap);
 
             Result result = new Result();
-            int len = result.getLengthList(llistalLength);
+            int len = result.getLengthList(llista);
             result.mostrarPercentatgeCaracter(hashMap, len, file);
         }
     }
