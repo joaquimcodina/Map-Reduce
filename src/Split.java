4ea05ed6e8283e1e133e7c622297d9470703b67a
joaquimcodina <joaquim.codina.espin@gmail.com>
2022-04-25 15:58:10 +0200

    Documentar i optimitzar Classe Split

--- src/Split.java
+++ src/Split.java
@@ -1,26 +1,40 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 
+/**
+ * Aquesta es la Classe Split
+ *
+ * @author Joaquim Codina (1566940) i Marc Cives (1563480)
+ *
+ */
 public class Split {
-    ArrayList<String> splitCharacter;
-    private int numLinies;
 
-    Split() {
-        splitCharacter = new ArrayList<>();
-        numLinies = 0;
-    }
-
-    public void actualitzarLista(ArrayList<String> llista) {
+    /**
+     * Aquesta es la funció generatesplitWord(), que consisteix en separar per paraules el contingut del ArrayList<String>,
+     * passat per paràmetre d'entrada.
+     *
+     * Utilitza un bucle foreach, que s'encarrega de reemplaçar els caràcters especials i a continuació, separar cadascuna de les linies
+     * del fitxer en paraules, i ho emmagatzema en un ArrayList<String> anomenat splitWord.
+     *
+     * @param llista ArrayList on tenim emmagatzemat el contingut del fitxer de text.
+     * @return ArrayList del contingut del fitxer de text, separat per paraules.
+     *
+     */
+    public ArrayList<String> generatesplitWord(ArrayList<String> llista) {
+        ArrayList<String> splitWord = new ArrayList<>();
         for (String s : llista) {
-            splitCharacter.add(Arrays.toString(s.toCharArray()));
+            String[] temp = s.toLowerCase()
+                    .replace(",", "")
+                    .replace(".", "")
+                    .replace(";", "")
+                    .replace("'", "")
+                    .replace("-", "")
+                    .replace(":", "")
+                    .replace("[", "")
+                    .replace("]", "")
+                    .split(" ");
+            splitWord.add(Arrays.toString(temp));
         }
+        return splitWord;
     }
-    public int getNumLinies() {
-        return numLinies;
-    }
-    public void setNumLinies(int numL) {
-        numLinies = numL;
-    }
-    public ArrayList<String> getArrayCharacters() { return splitCharacter; }
-
 }
