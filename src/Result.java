27aab6b9e5b3da899975c17fca7ac80de4dff009
joaquimcodina <joaquim.codina.espin@gmail.com>
2022-05-05 20:34:08 +0200

    Modificar Result.java

--- src/Result.java
+++ src/Result.java
@@ -1,3 +1,4 @@
+import java.io.File;
 import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -11,50 +12,38 @@ import java.util.HashMap;
 public class Result {
 
     /**
-     * Aquesta es la funció getLengthList(), consisteix obtenir la mida del fitxer de text (per paraules).
-     *
-     * @param llistaLength ArrayList on tenim emmagatzemat el contingut del fitxer de text.
-     * @return length, mida per paraules del ArrayList.
-     *
-     */
-    public int getLengthList(ArrayList<String> llistaLength){
-        int length=0;
-        for (String s : llistaLength) {
-            String[] temp = s.toLowerCase()
-                    .replace(",", "")
-                    .replace(".", "")
-                    .replace(";", "")
-                    .replace("'","")
-                    .replace("-", "")
-                    .replace(":", "")
-                    .split(" ");
-
-            length += temp.length;
-        }
-        return length;
-    }
-
-    /**
      * Aquesta es la funció mostrarPercentatgeCaracter(), bàsicament mostra el resultat obtingut per pantalla.
      *
-     * @param hashMap HashMap on tenim emmagatzemat els caracters amb el seu nombre d'aparicions en el fitxer de text.
+     * @param reduceThreads HashMap on tenim emmagatzemat els caracters amb el seu nombre d'aparicions en el fitxer de text.
      * @param len Mida del fitxer de text (per paraules).
      * @param file Nom del fitxer de text.
      *
      */
-    public void mostrarPercentatgeCaracter(HashMap<String, ArrayList<Integer>> hashMap, int len, String file){
-        double percentatge=0.0;
+    public void mostrarPercentatgeCaracter(ArrayList<Reduce> reduceThreads, int len, String file){
         DecimalFormat df = new DecimalFormat("0.00");
-
-        System.out.println(file + ":");
-
-        for (String key : hashMap.keySet()) {
-            for (double valors : hashMap.get(key)) {
-                percentatge = (valors / len) * 100;
+        HashMap<String, Integer> hashMap = new HashMap<>();
+        for (Reduce reduce : reduceThreads) {
+            for (String key : reduce.getHashMap().keySet()) {
+                hashMap.put(key, reduce.getHashMap().get(key));
             }
-            System.out.println(key + " : " + df.format(percentatge) + "%");
+        }
+
+        File fitxer = new File(file);
+        System.out.println(fitxer.getName() + ":");
+        for (String key : hashMap.keySet()) {
+            System.out.println(key + " : " + df.format((Double.valueOf(hashMap.get(key)) / len) * 100) + "%");
         }
         System.out.println("\n");
 
+        for(Reduce reduce : reduceThreads){
+            for(String caracter : reduce.llistaCaracters){
+                File fileCaracter = new File("key_"+caracter+".txt");
+                fileCaracter.delete();
+                for(String valueCaracter : reduce.llistaCaracters) {
+                    File fileValueCaracter = new File("value_"+valueCaracter+".txt");
+                    fileValueCaracter.delete();
+                }
+            }
+        }
     }
 }
