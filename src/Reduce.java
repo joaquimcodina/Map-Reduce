c4da6061cecd0fe52397b0b648c03b4186f6472c
joaquimcodina <joaquim.codina.espin@gmail.com>
2022-05-05 20:33:57 +0200

    Modificar Reduce.java

--- src/Reduce.java
+++ src/Reduce.java
@@ -1,3 +1,4 @@
+import java.io.*;
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -9,47 +10,54 @@ import java.util.HashMap;
  */
 public class Reduce extends Thread{
 
+    ArrayList<String> llistaCaracters;
+    HashMap<String, Integer> hashMap;
+    public Reduce(ArrayList<String> llistaCaracters) {
+        this.llistaCaracters = llistaCaracters;
+        hashMap = new HashMap<>();
+    }
+
+    @Override
+    public void run() {
+        this.generarReducing();
+    }
+
+    public void setHashMap(HashMap<String, Integer> hashMap){
+        this.hashMap = hashMap;
+    }
+
+    public HashMap<String, Integer> getHashMap() {
+        return this.hashMap;
+    }
+
     /**
      * Aquesta es la funció generarReducing(), bàsicament s'encarrega d'obtenir el HashMap amb els caràcters i el seu
      * nombre d'aparicions, i fer la suma del nombre d'aparicions de cada caràcter.
      *
-     * @param hashMap que hem obtingut de la funció generarShuffling() de la Classe Shuffle, on tenim emmagatzemat
-     *               els caràcters amb el seu nombre d'aparicions (sense sumar).
-     *
-     * @return hashMap on tenim emmagatzemat els caràcters amb el seu nombre d'aparicions dins del fitxer de text.
+     * El hashMap que hem obtingut de la funció generarShuffling() de la Classe Shuffle, on tenim emmagatzemat
+     * els caràcters amb el seu nombre d'aparicions (sense sumar).
      *
      */
-    private HashMap<String, ArrayList<Integer>> hashAux;
-    private HashMap<String, ArrayList<Integer>> hashRetorn;
-
-    Reduce() {
-        hashRetorn = new HashMap<>();
-    }
-
-    public void run() {
-        this.generarReducing(hashAux);
-    }
-
-    public void setHashAux(HashMap<String, ArrayList<Integer>> aux) {
-        hashAux = aux;
-    }
-
-    public HashMap<String, ArrayList<Integer>> getHashAux() {
-        return hashRetorn;
-    }
-
-    public void generarReducing(HashMap<String, ArrayList<Integer>> hashMap){
-
-        for (String key : hashMap.keySet()) {
-            Integer suma=0;
-            ArrayList<Integer> resultat = hashMap.get(key);
-            for (Integer valors : hashMap.get(key)){
-                suma += valors;
+    public void generarReducing(){
+        try {
+            BufferedReader readerValueCaracter;
+            int suma=0;
+            String encoding = "UTF-8";
+            for(String caracter : llistaCaracters){
+                File fileValueCaracter = new File("value_"+caracter+".txt");
+                FileInputStream fileValueInputStream = new FileInputStream(fileValueCaracter);
+                readerValueCaracter = new BufferedReader(new InputStreamReader(fileValueInputStream, encoding));
+                for (String value; (value = readerValueCaracter.readLine()) != null; ) {
+                    suma += Integer.parseInt(value);
+                }
+                hashMap.put(caracter, suma);
+                readerValueCaracter.close();
+                fileValueInputStream.close();
+                suma = 0;
             }
-            resultat.clear();
-            resultat.add(suma);
-            hashMap.put(key, resultat);
+
+        } catch (IOException e) {
+            System.out.println("Error: " + e);
         }
-        hashRetorn = hashMap;
     }
 }
