74d38cd40c37e8a60e2785dd0a5ab4b8ba19968b
joaquimcodina <joaquim.codina.espin@gmail.com>
2022-05-05 20:33:47 +0200

    Modificar Shuffle.java

--- src/Shuffle.java
+++ src/Shuffle.java
@@ -1,6 +1,5 @@
+import java.io.*;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.LinkedHashMap;
 
 /**
  * Aquesta es la Classe Shuffle
@@ -8,33 +7,59 @@ import java.util.LinkedHashMap;
  * @author Joaquim Codina (1566940) i Marc Cives (1563480)
  *
  */
-public class Shuffle {
+public class Shuffle extends Thread{
+
+    String fileName;
+    ArrayList<String> llistaCaracters;
+    public Shuffle(String fileName, ArrayList<String> llistaCaracters) {
+        this.llistaCaracters = llistaCaracters;
+        this.fileName = fileName;
+    }
+
+    @Override
+    public void run() {
+        this.generarShuffling();
+    }
+
+    public ArrayList<String> getLlistaCaracters(){
+        return this.llistaCaracters;
+    }
 
     /**
      * Aquesta es la funció generarShuffling(), bàsicament s'encarrega de generar un HashMap, i a partir d'un bucle
      * buscarà per cada caràcter, el nombre de vegades que apareix en el fitxer de text, en cas de que aparegui, afegim un 1
      * a la ArrayList<Integer> que es troba dins del HashMap, i que més endavant sumarem tot el seu contingut.
      *
-     * @param llista on s'emmagatzema el contingut del fitxer de text, el qual hem esborrat els caràcters repetits per paraules,
+     * llista on s'emmagatzema el contingut del fitxer de text, el qual hem esborrat els caràcters repetits per paraules,
      *              i l'hem separat per caràcters amb un split.
      *
-     * @return hashMap on s'emmagatzema els caràcters del fitxer juntament amb el seu nombre d'aparicions (sense sumar).
+     * hashMap on s'emmagatzema els caràcters del fitxer juntament amb el seu nombre d'aparicions (sense sumar).
      *
      */
-    public HashMap<String, ArrayList<Integer>> generarShuffling(ArrayList<String> llista) {
-        HashMap<String, ArrayList<Integer>> hashMap = new LinkedHashMap<>();
-        for (int i = 0; i < llista.size(); i++) {
-            ArrayList<Integer> valuesList = hashMap.get(llista.get(i));
-            if (!llista.get(i).equalsIgnoreCase("")) {
-                if(valuesList == null) {
-                    valuesList = new ArrayList<>();
-                    valuesList.add(1);
-                } else {
-                    valuesList.add(1);
+    public void generarShuffling() {
+        try{
+            File fitxerSplit = new File(fileName);
+            BufferedReader readerShuffle;
+            BufferedWriter writer;
+            String encoding = "UTF-8";
+            for (String caracter : llistaCaracters) {
+                FileInputStream shuffleInputStream = new FileInputStream(fitxerSplit);
+                readerShuffle = new BufferedReader(new InputStreamReader(shuffleInputStream, encoding));
+                writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("value_"+caracter+".txt", true), encoding));
+                for (String line; (line = readerShuffle.readLine()) != null;) {
+                    if (line.contains(caracter)) {
+                        writer.write("1");
+                        writer.newLine();
+                    }
                 }
-                hashMap.put(llista.get(i), valuesList);
+                readerShuffle.close();
+                shuffleInputStream.close();
+                writer.close();
             }
+            fitxerSplit.delete();
         }
-        return hashMap;
+        catch (IOException e){
+            System.out.println("Error: " + e);
+        }
     }
 }
