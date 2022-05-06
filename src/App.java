ba889309548a85bf470367101b269cdbead1c855
joaquimcodina <joaquim.codina.espin@gmail.com>
2022-05-06 15:17:36 +0200

    Modificar App

--- src/App.java
+++ src/App.java
@@ -1,5 +1,10 @@
-import java.io.FileNotFoundException;
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Aquesta es la Classe principal, on fem crides a les diferents funcions de les altres classes per dur a terme
@@ -9,22 +14,22 @@ import java.util.ArrayList;
  *
  */
 public class App {
-    public static void main(String[] args) throws InterruptedException, FileNotFoundException {
+    public static void main(String[] args) throws InterruptedException, IOException {
 
         long inicio = System.currentTimeMillis();
         for (String file : args) {
             ArrayList<String> llistaFitxers = new ArrayList<>();
+            Set<String> llistaCaracters = new HashSet<>();
             ArrayList<Map> mapThreads = new ArrayList<>();
-            ArrayList<Shuffle> shuffleThreads = new ArrayList<>();
+            ArrayList<Shuffle> shuffles = new ArrayList<>();
             ArrayList<Reduce> reduceThreads = new ArrayList<>();
             Entry entry = new Entry();
-            //Shuffle shuffle = new Shuffle();
             Split split = new Split();
             Result result = new Result();
-            int len=0, max_lines=10000, count=0;
+            int len=0, count=0;
 
             System.out.print("FASE ENTRY ");
-            llistaFitxers = entry.generarFitxers(file, max_lines, llistaFitxers);
+            llistaFitxers = entry.generarFitxers(file, 10000, llistaFitxers);
             System.out.println("OK");
 
             System.out.print("FASE SPLIT ");
@@ -41,17 +46,26 @@ public class App {
             for(Map map : mapThreads){
                 map.join();
                 len += map.getLengthFitxer();
-                shuffleThreads.add(new Shuffle(map.getFileName(), map.getLlistaCaracters()));
+                llistaCaracters.addAll(map.getLlistaCaracters());
             }
             System.out.println("OK");
+            BufferedWriter keys = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("keys.txt")));
+            for(String caracter : llistaCaracters){
+                keys.write(caracter);
+                keys.newLine();
+                for(Map map : mapThreads){
+                    shuffles.add(new Shuffle(map.getFileName(), caracter));
+                }
+            }
+            keys.close();
 
             System.out.print("FASE SHUFFLE ");
-            for(Shuffle shuffle : shuffleThreads){
+            for (Shuffle shuffle : shuffles) {
                 shuffle.start();
             }
-            for(Shuffle shuffle : shuffleThreads){
+            for (Shuffle shuffle : shuffles) {
                 shuffle.join();
-                reduceThreads.add(new Reduce(shuffle.getLlistaCaracters()));
+                reduceThreads.add(new Reduce(shuffle.getFileName(), shuffle.getCaracter()));
             }
             System.out.println("OK");
 
@@ -63,11 +77,7 @@ public class App {
                 reduce.join();
             }
             System.out.println("OK\n");
-
-            result.mostrarPercentatgeCaracter(reduceThreads, len, file);
-            reduceThreads.clear();
-            shuffleThreads.clear();
-            mapThreads.clear();
+            result.mostrarPercentatgeCaracter(len, file);
         }
         long fin = System.currentTimeMillis();
         double tiempo = (double) ((fin - inicio)/1000);
